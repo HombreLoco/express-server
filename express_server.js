4d291e25aae2501ed55864175d1abc5cbad57fbd
Jacques Begin <jacques.andre.begin@gmail.com>
2017-04-12 00:33:04 +0000

    Adds form to sumbit URLs, page to display all converted URLs, and a single URL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,14 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,18 +17,44 @@ var urlDatabase = {
 
 
 // app.get calls are routes to handle reponses given requested parameters
-
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
+
+// this route receives form submission data for converting long URLs to
+// short URLs
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
+// this route renders a view to display all URLs and their shortened forms
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+// this route renders a view for users to enter full URLs to a form to
+// be converted to short URLs by the server
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+
+
+// this route renders a view to display a single URL and it's shortened form
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+})
+
 
 // this route sends back HTML code in the response
-
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
